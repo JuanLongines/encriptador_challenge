7de49d8934c1ecd11c61aa690ec596e9858a91ee
JCarlos <dev_solsoft@aol.com>
2024-03-11 12:55:40 -0600

    OK

--- docs/js/encriptador.js
+++ docs/js/encriptador.js
@@ -2,37 +2,58 @@ const caja_texto=document.querySelector("#caja_texto");
 const avisoVacio=document.querySelector(".aviso_vacio");
 const mensajeEncriptadoJs=document.querySelector(".mensaje_encriptado");
 const botonCopiarJs=document.querySelector(".btn_copiar");
+const copyTextareaBtn = document.querySelector('.btn_copiar');
 
 let textoEncriptado='';
-console.log('avisoVacio',avisoVacio)
-const encriptacionMapa = {
-    'e': 'enter',
-    'i': 'imes',
-    'a': 'ai',
-    'o': 'ober',
-    'u': 'ufat'
-};
 
-const desencriptacionMapa = {
-    'enter': 'e',
-    'imes': 'i',
-    'ai': 'a',
-    'ober': 'o',
-    'ufat': 'u'
-};
+function encriptar(textoEncriptado){
+    let matriz=[
+        ["e","enter"],
+        ["o","ober"],
+        ["u","ufat"],
+        ["i","imes"],
+        ["a","ai"]
+    ]
+    textoEncriptado=textoEncriptado.toLowerCase();
+
+    for (let i=0; i<matriz.length; i++) {
+        if (textoEncriptado.includes(matriz[i][0])){
+            textoEncriptado=textoEncriptado.replaceAll(matriz[i][0],matriz[i][1]);
+        }
+    }
+
+    return textoEncriptado;
+}
+
+function desencriptar(textoDesencriptado){
+    let matriz=[
+        ["u","ufat"],
+        ["o","ober"],
+        ["e","enter"],
+        ["i","imes"],
+        ["a","ai"]
+    ]
+    textoDesencriptado=textoDesencriptado.toLowerCase();
+
+    for (let i=0; i<matriz.length; i++) {
+        if (textoDesencriptado.includes(matriz[i][1])){
+            textoDesencriptado=textoDesencriptado.replaceAll(matriz[i][1],matriz[i][0]);
+        }
+    }
+
+    return textoDesencriptado;
+}
 
 
 comprobarSiMensajeExiste();
 function encriptarTexto(){
-    textoEncriptado= caja_texto.value.split('').map(char => encriptacionMapa[char] || char).join('');
+    textoEncriptado= encriptar(caja_texto.value);
     comprobarSiMensajeExiste();
     caja_texto.value='';
 }
 
 function desencriptarTexto(){
-    console.log('caja: ',caja_texto.value)
-    textoEncriptado= caja_texto.value.split(/(enter|imes|ai|ober|ufat)/).map(word => desencriptacionMapa[word] || word).join('');
-    console.log('Hoberlai',textoEncriptado)
+    textoEncriptado= desencriptar(caja_texto.value);
     comprobarSiMensajeExiste();
     caja_texto.value='';
 }
@@ -62,25 +83,23 @@ function copiarTextoEncriptado(){
 
 }
 
-var copyTextareaBtn = document.querySelector('.btn_copiar');
 
-copyTextareaBtn.addEventListener('click', function(event) {
+
+
+copyTextareaBtn.addEventListener("click", async (event) => {
     var copyTextarea = document.querySelector('.mensaje_encriptado');
     var text = copyTextarea.textContent;
     var tempTextarea = document.createElement('textarea');
-    //tempTextarea.style.position = 'absolute';
-    //tempTextarea.style.left = '-9999px';
     tempTextarea.value = text;
     document.body.appendChild(tempTextarea);
     tempTextarea.select();
 
     try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Copying text command was ' + msg);
+        await navigator.clipboard.writeText(text);
+        console.log('Copying to clipboard was successful!');
     } catch (err) {
         console.log('Oops, unable to copy');
     }
     document.body.removeChild(tempTextarea);
-
 });
+
